9ce99b70eafd8ab2d72793278d32002d98667aa2
wuuconix <1521900139@qq.com>
2022-09-26 21:43:21 +0800

    full import elementPlus

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const src = ref("https://tva2.sinaimg.cn/large/007YVyKcly1h5mmf47su9j30ma0mlq7v.jpg")
+const url = ref("https://tva2.sinaimg.cn/large/007YVyKcly1h5mmf47su9j30ma0mlq7v.jpg")
+const srcList = ref(["https://tva2.sinaimg.cn/large/007YVyKcly1h5mmf47su9j30ma0mlq7v.jpg"])
 </script>
 
 <template>
-  <img :src="src">
+  <el-image
+    :src="url"
+    :preview-src-list="srcList"
+    fit="cover"
+  />
 </template>
 
 <style scoped>
-img {
+.el-image {
   width: 200px;
 }
 </style>
